80ad0082b3d268112a973ab3c7f493669744c07a
79176 <79176@LAPTOP-MN0I58DS>
2023-09-07 19:33:02 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static double radius;
+        private static double len;
+        private static double square;
+        static void Main(string[] args)
+        { 
+        
+        radius = double.Parse(Console.ReadLine());
+            len = 3.14 *2.0* radius;
+            square = 3.14 * Math.Pow(radius,2.0);
+            Console.WriteLine("Длина окружности:" + len);
+            Console.WriteLine("Площадь окружности:" + square);
+
+        }
+    }
+
+}
